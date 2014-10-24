623e421e3e738ba398921094dbb9ba6a673793f2
colin-ch <colinhamer@casenine.com>
2014-10-24 12:12:03 +0200

    Deleted dependency on Jquery

--- background.js
+++ background.js
@@ -1,10 +1,38 @@
-var appended = false;
-$('body').on('click', '.button_with_field', function (event) {
+var observer = new MutationObserver(function (mutations) {
+	var form = document.querySelector('form.story:not(.casenine)');
+	if (form === null) {
+		return;
+	}
+	var actions = form.querySelector('.actions');
 
-  $(this).parent().prepend("<button type='button' class='clipboard_button' style='padding: 2px 2px 6px 2px;' data-clipboard-text='git commit -m &quot;" + $(this).closest(".story.model.item" ).find(".story.name textarea").val() + " [Delivers #" + $(this).closest(".story.model.item" ).data("id") + "]&quot;'><img src='http://www.peterhajas.com/media/img/github_icon.png' width='16px' height='16px' /></button>");
+	var story = closest(form, '.story.model.item');
+	var name = story.querySelector('.story.name textarea').value;
+	var id = story.dataset.id;
 
+	var button = document.createElement('button');
+	button.type = 'button';
+	button.style.padding = '2px 2px 6px 2px';
+	button.classList.add('clipboard_button');
+	button.dataset.clipboardText = name + ' [Delivers #' + id + ']';
+	button.innerHTML = '<img src="http://www.peterhajas.com/media/img/github_icon.png" width="16px" height="16px" />';
+
+	actions.insertBefore(button, actions.firstChild);
+
+	form.classList.add('casenine');
 });
+observer.observe(document, {childList: true, subtree: true});
 
 
 
+function closest(el, selector) {
+    // traverse parents
+    while (el !== null) {
+        var parent = el.parentElement;
+        if (parent !== null && parent.matches(selector)) {
+            return parent;
+        }
+        el = parent;
+    }
 
+    return null;
+}
